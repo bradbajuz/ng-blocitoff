e48166c578e80d89bc53bd6acb3e733293ff911d
Brad Ballard <brad@bradballard.com>
2014-10-12 16:05:22 -0400

    Start to get AngularJS working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('blocitoff', []);
+bloccitoff = angular.module('Bloccitoff', ['ui.router']);
+
+bloccitoff.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'Landing.controller',
+    templareUrl: '/templates/landing.html'
+  });
+}]);
+
+bloccitoff.controller('Landing.controller', ['$scope', function($scope) {
+
+}]);
