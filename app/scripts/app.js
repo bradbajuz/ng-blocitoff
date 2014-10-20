1a367b4bb90733651bf1e176bb953e933b85eb89
Brad Ballard <brad@bradballard.com>
2014-10-20 08:21:51 -0400

    Start implementation of expired tasks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,14 +14,21 @@ blocitoff.controller('Landing.controller', ['$scope', function($scope) {
   $scope.header = "A self-destructing todo list.";
 
   $scope.items = [
-    {title: 'Learn Haskell', days: 7, complete: false},
+    {title: 'Learn Haskell', days: 7, complete: false, expired: false},
     {title: 'Take out trash', days: 3, complete: false},
-    {title: 'Get beer', days: 1, complete: false}
+    {title: 'Get beer', days: 1, complete: false},
+    {title: 'Learn to program', days: 90, complete: false, expired: true}
   ];
   
   $scope.addTodo = function () {
     $scope.items.push({title: $scope.todoTitle, days: 7, complete: false})
     $scope.todoTitle = "";
+  }
+
+  $scope.expiredTodo = function () {
+    if ($scope.items.days > 7) {
+      
+    }
   }
     
 }]);
